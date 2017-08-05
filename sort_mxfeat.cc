1ef9e39ee5d648b2251c100b29f35bfe4bc9ab8d
王春棋（Chunqi.Wang） <sunbirdinthesky@users.noreply.github.com>
2017-08-05 19:07:49 +0800

    add log system, changed the user guide

--- sort_mxfeat.cc
+++ sort_mxfeat.cc
@@ -1,3 +1,8 @@
+/************************
+ * author : chunqi.wang@hobot.cc
+ * time   : 2017-8-5
+ ************************/
+
 #include <fstream>
 #include <iostream>
 #include <algorithm> 
@@ -56,6 +61,7 @@ void get_item_info (const string path, priority_queue <item_info> &sorted_fileli
     fin.seekg(info.data_size, ios::cur);
   }
   fin.close();
+  cout << "reading now, total " << sorted_filelist.size() << " mxfeat items have been read" << endl;
 }
 
 void write_file (const vector<item_info> itemlist, const int items_per_file, const string dest_dir, const string prefix) {
@@ -74,7 +80,6 @@ void write_file (const vector<item_info> itemlist, const int items_per_file, con
     info = itemlist[i];
     uint32_t magic_number    = 0xced7230a;
     uint32_t flag_and_length = (info.cflag << 29U) | info.data_size;
-    cout << hex << magic_number << flag_and_length << endl;
     fin.open(info.path, ios::binary);
     fin.seekg(info.address, ios::beg);
     fin.read(buf, info.data_size);
@@ -87,10 +92,12 @@ void write_file (const vector<item_info> itemlist, const int items_per_file, con
 
     if (n_items % items_per_file == 0) {
       out_file_id += 1;
+//      cout << "items " << prefix << " finished " << n_items*100/1.0/len << "%" << endl;
       fout.close();
       if (n_items != len) fout.open(dest_dir + '/' + prefix + "id_" + to_string(out_file_id), ios::binary);
     }
   }
+  cout << "items  " << prefix << " finished 100%" << endl;
   fout.close();
 }
 
@@ -112,10 +119,13 @@ void rebuild_mxfeats (
   if (ptr_parts == n_points-1) prefix += "max_";
   else prefix += to_string(split_points[ptr_parts]) + '_';
 
+  cout << endl;
+  cout << "job started, total " << locallist.size() << " mxfeat items" << endl;
   while (!locallist.empty()) {
     info = locallist.top();
     while (info.data_size > split_points[ptr_parts]*(feats_dim+1)*4) {
       write_file(parts[ptr_parts], items_per_file, dest_dir, prefix);
+      cout << "total progress finished " << (1-(locallist.size()/1.0/sorted_filelist.size()))*100 << "%" << endl <<endl;
       ptr_parts++;
 
       prefix = "length_" + to_string(split_points[ptr_parts-1]) + '_';
@@ -126,6 +136,7 @@ void rebuild_mxfeats (
     locallist.pop();
   }
   write_file(parts[ptr_parts], items_per_file, dest_dir, prefix);
+  cout << "job finished, have a nice day o(^.^o)" << endl;
 }
 
 int main (int argcnt, char **argval) {
@@ -136,8 +147,11 @@ int main (int argcnt, char **argval) {
     cout << "note : if split point is 200 and 300, the item(data chunk) whose length is smaller than 200 will                   " << endl;
     cout << "       be saved in file \"length0_200\", length between 200 and 300 will be saved in \"length200_300\"             " << endl;
     cout << "       the others will be saved in \"length300_650000000\"                                                         " << endl;
-    cout << "warning : only support 4-bit label and 4-bit feats data. which means label data type hould be uint32_t             " << endl;
-    cout << "          and feats data type should be float32. if not, data set will be destroyed and can`t be used any more     " << endl;
+    cout << "warning : 1. only support 4-bit label and 4-bit feats data. which means label data type should be uint32_t         " << endl;
+    cout << "             and feats data type should be float32. if not, data set will be destroyed and can`t be used any more  " << endl;
+    cout << "          2. if dim is not correct, program will NOT classify item`s frame length correctly. For example, an item  " << endl;
+    cout << "             with real frame quantity 400 will be classified to 200 frames data set if you give the program        " << endl;
+    cout << "             a 2 times larger dim than its real dim. but output data set still could be used.                      " << endl;
     return 0;
   }
   if (opendir(argval[1]) == NULL) {
@@ -148,6 +162,8 @@ int main (int argcnt, char **argval) {
     cout << "ERROR : dest_dir not exist" << endl;
     return 0;
   }
+  if (atoi(argval[4]) != 40 && atoi(argval[4]) != 60 && atoi(argval[4]) != 80)
+    cout << "feats dim is not 40, 60 or 80, ARE YOU SURE?" << endl;
 
   vector <string> filelist = get_filelist(argval[1]);
   priority_queue <item_info> sorted_filelist; 
